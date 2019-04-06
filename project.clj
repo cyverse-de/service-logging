fbbdbde37137e516d14d2c144fd25bebefc6a722
Sarah Roberts <sarahr@cyverse.org>
2019-04-05 17:39:03 -0700

    bumped the snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/service-logging "2.8.1"
+(defproject org.cyverse/service-logging "2.8.2-SNAPSHOT"
   :description "Common Logging Utilities for Clojure Projects"
   :url "https://github.com/cyverse-de/service-logging"
   :license {:name "BSD"
