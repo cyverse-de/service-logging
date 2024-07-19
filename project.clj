e8a094d6ef256101af5cc1f4dbcac8159988dead
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-19 10:53:04 -0700

    update the version to 2.8.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/service-logging "2.8.4"
+(defproject org.cyverse/service-logging "2.8.5-SNAPSHOT"
   :description "Common Logging Utilities for Clojure Projects"
   :url "https://github.com/cyverse-de/service-logging"
   :license {:name "BSD"
